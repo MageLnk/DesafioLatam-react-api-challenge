aeec2bbc4d311c0e32a0b62d1c64611ba7ec6e32
MageLink <mage.link.zhezard@gmail.com>
2022-09-10 13:14:50 -0300

    BrowserRouter completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import { useContext } from "react";
+//import { useContext } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 // Context
-import GeneralContext from "./Context/GeneralContext";
+//import GeneralContext from "./Context/GeneralContext";
+// Components
+import { Home } from "./pages";
 
 const App = () => {
-  const { logIn } = useContext(GeneralContext);
-  console.log(logIn);
-  return <div>App</div>;
+  //const { logIn } = useContext(GeneralContext);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>Home o k ase</div>;
+};
+
+export default Home;
