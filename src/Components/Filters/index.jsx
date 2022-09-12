e7007c45443d776fc90eb1be7722b53c54e073ac
MageLink <mage.link.zhezard@gmail.com>
2022-09-12 10:05:49 -0300

    Filters are Working and reactive. Switch added to do more realistic behavior

---
+++ src/Components/Filters/index.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+
+// Style
+import { BsFilter } from "react-icons/bs";
+
+const Filters = ({ filteringData }) => {
+  const [topFilterData, setTopFilterData] = useState(false);
+  const [nameFilterNada, setNameFilterData] = useState(true);
+  const [YearFilterNada, setYearFilterData] = useState(true);
+
+  const fixFilters = ({ selection }) => {
+    switch (selection) {
+      case "Top":
+        setNameFilterData(true);
+        setYearFilterData(true);
+        break;
+      case "Name":
+        setTopFilterData(true);
+        setYearFilterData(true);
+        break;
+      case "Year":
+        setTopFilterData(true);
+        setNameFilterData(true);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className="view-more-filters-container">
+      <span className="view-more-title">Los mejores animes</span>
+      <div className="view-more-filters">
+        <span
+          className="view-more-filter"
+          onClick={() => {
+            setTopFilterData(!topFilterData);
+            filteringData({ topFilterData, selection: "Top" });
+            fixFilters({ selection: "Top" });
+          }}
+        >
+          Top animé{" "}
+          <BsFilter
+            viewBox={!topFilterData ? "0 0 16 08" : "0 4 16 16"}
+            className={!topFilterData ? "view-more-icon-filter" : "view-more-icon-filter-rotate"}
+          />
+        </span>
+        <span
+          className="view-more-filter"
+          onClick={() => {
+            setNameFilterData(!nameFilterNada);
+            filteringData({ nameFilterNada, selection: "Name" });
+            fixFilters({ selection: "Name" });
+          }}
+        >
+          Nombre{" "}
+          <BsFilter
+            viewBox={!nameFilterNada ? "0 0 16 08" : "0 4 16 16"}
+            className={!nameFilterNada ? "view-more-icon-filter" : "view-more-icon-filter-rotate"}
+          />
+        </span>
+        <span
+          className="view-more-filter"
+          onClick={() => {
+            setYearFilterData(!YearFilterNada);
+            filteringData({ YearFilterNada, selection: "Year" });
+            fixFilters({ selection: "Year" });
+          }}
+        >
+          Año{" "}
+          <BsFilter
+            viewBox={!YearFilterNada ? "0 0 16 08" : "0 4 16 16"}
+            className={!YearFilterNada ? "view-more-icon-filter" : "view-more-icon-filter-rotate"}
+          />
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Filters;
--- src/Components/ViewMoreContent/index.jsx
+++ src/Components/ViewMoreContent/index.jsx
@@ -1,15 +1,10 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 // Context
 import GeneralContext from "../../Context/GeneralContext";
 // Components
-import { Loading, ShowCard } from "../index";
-// Style
-import { BsFilter } from "react-icons/bs";
+import { Loading, ShowCard, Filters } from "../index";
 
 const ViewMoreContent = () => {
-  const [topFilterData, setTopFilterData] = useState(false);
-  const [nameFilterNada, setNameFilterData] = useState(true);
-  const [YearFilterNada, setYearFilterData] = useState(true);
   const { dataTopOnLoad } = useContext(GeneralContext);
 
   //console.log("Viendo el dataTopOnLoad", dataTopOnLoad.data);
@@ -21,56 +16,7 @@ const ViewMoreContent = () => {
   return (
     <div className="main-content-container">
       <div className="view-more-container">
-        <div className="view-more-filters-container">
-          <span className="view-more-title">Los mejores animes</span>
-          <div className="view-more-filters">
-            <span
-              className="view-more-filter"
-              onClick={() => {
-                setTopFilterData(!topFilterData);
-                filteringData({ topFilterData, selection: "Top" });
-              }}
-            >
-              Top animé{" "}
-              <BsFilter
-                viewBox={!topFilterData ? "0 0 16 08" : "0 4 16 16"}
-                className={
-                  !topFilterData ? "view-more-icon-filter" : "view-more-icon-filter-rotate"
-                }
-              />
-            </span>
-            <span
-              className="view-more-filter"
-              onClick={() => {
-                setNameFilterData(!nameFilterNada);
-                filteringData({ nameFilterNada, selection: "Name" });
-              }}
-            >
-              Nombre{" "}
-              <BsFilter
-                viewBox={!nameFilterNada ? "0 0 16 08" : "0 4 16 16"}
-                className={
-                  !nameFilterNada ? "view-more-icon-filter" : "view-more-icon-filter-rotate"
-                }
-              />
-            </span>
-            <span
-              className="view-more-filter"
-              onClick={() => {
-                setYearFilterData(!YearFilterNada);
-                filteringData({ YearFilterNada, selection: "Name" });
-              }}
-            >
-              Año{" "}
-              <BsFilter
-                viewBox={!YearFilterNada ? "0 0 16 08" : "0 4 16 16"}
-                className={
-                  !YearFilterNada ? "view-more-icon-filter" : "view-more-icon-filter-rotate"
-                }
-              />
-            </span>
-          </div>
-        </div>
+        <Filters filteringData={filteringData} />
         <div className="view-more-cards-container">
           {!dataTopOnLoad ? <Loading /> : <ShowCard totalData={dataTopOnLoad.data} />}
         </div>
